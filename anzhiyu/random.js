b50766b6b07cb1cedd29cbb305a57e1521f42540
morepaperac <jasonaowu@163.com>
2024-06-05 15:18:53 +0800

    Site updated: 2024-06-05 15:18:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/29/hello-world/","2024/01/29/算法/数据结构/","2024/01/29/算法/leetcode/","2024/06/05/RL/MDP/"];function toRandomPost(){
+var posts=["2024/01/29/hello-world/","2024/01/29/算法/数据结构/","2024/06/05/RL/MDP/","2024/01/29/算法/leetcode/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
